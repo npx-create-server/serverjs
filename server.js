a2f5b4d54125614df5e928f426dcc5464ed5cb99
kmarellano <kurt.arellano@stratpoint.com>
2022-12-15 19:23:01 +0800

    added error handling and readme

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const app = require('./startup/app');
 const db = require('./startup/db');
 require('./routers')(app);
 
+app.use((error, req, res, next) => {
+    const { statusCode, message } = error;
+    statusCode = statusCode || 500;
+
+    return res.status(statusCode).json({ message });
+});
+
 const port = process.env.PORT;
 const start = async () => {
     await db();
